1b0951ffa52b7ec82e29776acd54062d42c1d147
NatGraff <97193253+NatGraff@users.noreply.github.com>
2022-07-27 14:26:35 -0300

    img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    RedesComponent
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,

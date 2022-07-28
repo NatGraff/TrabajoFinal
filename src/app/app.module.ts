9a71830fc1b443bca2afe8dcebcd2cdb3fd3fd2d
NatGraff <97193253+NatGraff@users.noreply.github.com>
2022-07-28 17:57:12 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoApComponent,
     RedesComponent,
-    BannerComponent
+    BannerComponent,
+    SobreMiComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
